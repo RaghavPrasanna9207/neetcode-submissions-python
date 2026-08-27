78e13e3b880ccb315a0eeb33438ffca19ff74e8e
Raghav Prasanna <raghavprasanna09@gmail.com>
2026-08-27 23:44:11 +0530

    Update: non-overlapping-intervals - submission-0

--- Data Structures & Algorithms/non-overlapping-intervals/submission-0.py
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-0.py
@@ -1,19 +1,15 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        # sort and go through each interval. if theres an overlap, delete the one which 
-        # ends later (greedy)
-
+        # Sort by interval start, if there is a merge, append the count and take the minimum of the current ending and the previous ending, as this greedy method gives us lesser chances of more collisions in the future.
+        # Complexities: O(n log n), O(1 or n) depending on the sorting algorithm.
         intervals.sort()
         count = 0
         prevEnd = intervals[0][1]
 
         for start, end in intervals[1:]:
             if start < prevEnd:
-                # starts before old interval ends - overlap
                 count += 1
-                prevEnd = min(end, prevEnd) # update the end value accordingly
-                # here we do not delete anything as the problem requires counting
-                # choosing the minimum end value reduces probability of later merges
+                prevEnd = min(end, prevEnd)
             else:
                 prevEnd = end
         return count
